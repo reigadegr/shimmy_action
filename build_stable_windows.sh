fcae5df26359e7488440be8f2d3661a53cc458f0
reigadegr <2722688642@qq.com>
2025-09-08 22:19:09 +0800

    fix: 摆烂了

--- build_stable_windows.sh
+++ build_stable_windows.sh
@@ -4,18 +4,15 @@ export RUSTFLAGS="
     -C relro-level=none \
     -C code-model=small \
     -C linker-plugin-lto=no \
-    -C link-arg=/OPT:REF,ICF \
     -C symbol-mangling-version=v0 \
-    -C target-feature=+crt-static \
-    -C link-arg=libucrt.lib \
-    -C link-arg=/NODEFAULTLIB:ucrt \
-    -C link-arg=/NODEFAULTLIB:ucrtd \
-    -C link-arg=/NODEFAULTLIB:msvcrt \
+    -C link-arg=legacy_stdio_definitions.lib \
+    -C link-arg=/OPT:REF,ICF \
     -C llvm-args=-fp-contract=off \
     -C llvm-args=-enable-misched \
     -C llvm-args=-enable-post-misched \
     -C llvm-args=-enable-dfa-jump-thread
-" 
+"
+
 
 cargo update
 
