c735b4f7dc0536a2037ff97ca2318fa33cfae214
reigadegr <2722688642@qq.com>
2025-09-08 17:59:48 +0800

    Revert "fix: 解决符号重定义"
    This reverts commit ac4ad3ecf72dfd1ac21d9355bda242905fee91ef.

--- build_nightly_windows.sh
+++ build_nightly_windows.sh
@@ -21,6 +21,7 @@ export RUSTFLAGS="
     -C default-linker-libraries \
     -C target-feature=+crt-static \
     -C symbol-mangling-version=v0 \
+    -C link-arg=ucrt.lib \
     -C link-arg=legacy_stdio_definitions.lib \
     -C llvm-args=-fp-contract=off \
     -C llvm-args=-enable-misched \
