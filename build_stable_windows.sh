a173777ec8b00df82de4ad5e0f48f25b1847518b
reigadegr <2722688642@qq.com>
2025-10-12 09:01:51 +0800

    chore: 加-C target-cpu=native

--- build_stable_windows.sh
+++ build_stable_windows.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 export RUSTFLAGS="
-    -C relro-level=none \
-    -C code-model=small \
-    -C linker-plugin-lto=no \
-    -C symbol-mangling-version=v0 \
-    -C link-arg=legacy_stdio_definitions.lib \
-    -C link-arg=/OPT:REF,ICF \
-    -C llvm-args=-fp-contract=off \
-    -C llvm-args=-enable-misched \
-    -C llvm-args=-enable-post-misched \
+    -C relro-level=none
+    -C code-model=small
+    -C linker-plugin-lto=no
+    -C target-cpu=native
+    -C symbol-mangling-version=v0
+    -C link-arg=legacy_stdio_definitions.lib
+    -C link-arg=/OPT:REF,ICF
+    -C llvm-args=-fp-contract=off
+    -C llvm-args=-enable-misched
+    -C llvm-args=-enable-post-misched
     -C llvm-args=-enable-dfa-jump-thread
 "
 
